bb09d01f0d006a8341a682882cf64d278ff89c24
TheadoraTang <100395166+TheadoraTang@users.noreply.github.com>
2023-10-14 14:55:47 +0800

    Update init.py

--- init.py
+++ init.py
@@ -2,8 +2,8 @@ import pymongo
 
 def init_mongodb():
     # 连接到MongoDB数据库
-    client = pymongo.MongoClient('localhost', 27017)  # 请根据实际情况修改MongoDB的连接URL
-    db = client["bookstore"]  # 替换为您的数据库名称
+    client = pymongo.MongoClient('localhost', 27017) 
+    db = client["bookstore"]  # 数据库名称
 
     # 创建用户集合并添加索引
     users = db["users"]
