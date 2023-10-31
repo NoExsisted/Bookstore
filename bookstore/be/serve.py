c88ef1a2e568b66029e475b67ddaac408cfc3029
lzx <112930061+NoExsisted@users.noreply.github.com>
2023-10-31 11:43:21 +0800

    app启动不运行store

--- bookstore/be/serve.py
+++ bookstore/be/serve.py
@@ -1,4 +1,12 @@
-from be.view import auth, search
+'''
+Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+Date: 2023-10-26 18:25:22
+LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+LastEditTime: 2023-10-26 18:52:48
+FilePath: \bookstore\be\serve.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+from be.view import auth
 from be.view import seller
 from be.view import buyer
 from be.model.store import init_database
@@ -29,7 +37,7 @@ def be_run():
     # parent_path = os.path.dirname(this_path)
     # log_file = os.path.join(parent_path, "app.log")
 
-    init_database('127.0.0.1', 27017, 'bookstore')
+    #########init_database('127.0.0.1', 27017, 'bookstore')
     # logging.basicConfig(filename=log_file, level=logging.ERROR)
     # handler = logging.StreamHandler()
     # formatter = logging.Formatter(
